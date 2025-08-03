d0f67d64b23899e9fcc66b648e770fb9121ab1b8
Shariar076 <shariar1405076@gmail.com>
2025-08-03 23:43:23 +0600

    update: autoconvert to epub

--- download.py
+++ download.py
@@ -10,29 +10,45 @@ html_end = """
 </body>
 </html>
 """
+main_out_filename = "মনের মানুষ - সুনীল গঙ্গোপাধ্যায়"
 
-book_file = open(f"দুর্গেশনন্দিনী.html", 'w')
+book_file = open(f"{main_out_filename}.html", 'w')
 
 book_file.write(html_start)
 
 
 def get_page_soup(url):
-    page_response = requests.get(url).content
+    HEADER = {'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.1; SM-T395) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.83 Safari/537.36'}
+    PROXY = {"https": "https//59.110.7.190:1080"}
+    try:
+        page_response = requests.get(url, 
+                                    #  proxies=PROXY, 
+                                    #  headers=HEADER
+                                    timeout=5
+                                    ).content
+    except:
+        page_response = None
     return BeautifulSoup(page_response, 'html.parser')
 
 
 def download_chapters(book_title, chap_list):
     for chap_title, chap_addr in chap_list:
-        print(chap_title)
-        book_file.write(f"<h2>{chap_title}</h2>")
-        chap_soup = get_page_soup(chap_addr).find('div', {'class': 'entry-content'})
+        print(chap_title.text)
+        book_file.write(f"<h2>{chap_title.text}</h2>")
+        chap_soup = None
         while chap_soup is None:
-            print("Couldn't get Chapter")
-            sleep(30)
-            print("Trying again")
-            chap_soup = get_page_soup(chap_addr).find('div', {'class': 'entry-content'})
+
+            chap_soup = get_page_soup(chap_addr).find('div', {'class': 'entry-content'}).find_all('p')
+            if chap_soup is None:
+                print("Couldn't get Chapter")
+                sleep(30)
+                print("Trying again")
+            else:
+                print("Done")
+                sleep(1)
+                break
         # chap_soup.button.decompose()
-        book_file.write(str(chap_soup))
+        book_file.write('\n'.join([f"<p>{par.text}</p>" for par in chap_soup]))
         # with open(f'{book_title}/{chap_title}.xml', 'w') as soup_file:
         #     soup_file.write(str(chap_soup))
 
@@ -43,8 +59,10 @@ def download_book(book_addr, book_title):
     chap_list = []
     while True:
         book_soup = get_page_soup(book_addr_loc)
-        chap_list = chap_list + [[article.get('aria-label'), article.find('a', href=True).get('href')] for article in
-                                 book_soup.find_all('article')]
+        # chap_list = chap_list + [[article.get('aria-label'), article.find('a', href=True).get('href')] for article in
+        #                          book_soup.find_all('article')]
+        chap_list = chap_list + [[article.find(class_= 'ld-item-title'), article.find('a', href=True).get('href')] for article in
+                                 book_soup.find_all(class_='ld-item-list-item')]
 
         next = book_soup.find("li", class_="pagination-next")
         if next is not None:
@@ -52,6 +70,7 @@ def download_book(book_addr, book_title):
         else:
             break
     print(len(chap_list))
+    # print(chap_list)
     download_chapters(book_title, chap_list)
 
 
@@ -66,8 +85,11 @@ def download_book(book_addr, book_title):
 # for book_addr, book_title in book_addr_list:
 #     print(book_title)
 #     download_book(book_addr, book_title)
-book_addr = 'https://www.ebanglalibrary.com/category/%e0%a6%ac%e0%a6%99%e0%a7%8d%e0%a6%95%e0%a6%bf%e0%a6%ae%e0%a6%9a%e0%a6%a8%e0%a7%8d%e0%a6%a6%e0%a7%8d%e0%a6%b0-%e0%a6%9a%e0%a6%9f%e0%a7%8d%e0%a6%9f%e0%a7%8b%e0%a6%aa%e0%a6%be%e0%a6%a7%e0%a7%8d%e0%a6%af/%e0%a6%89%e0%a6%aa%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%b8-%e0%a6%ac%e0%a6%99%e0%a7%8d%e0%a6%95%e0%a6%bf%e0%a6%ae/%e0%a6%a6%e0%a7%81%e0%a6%b0%e0%a7%8d%e0%a6%97%e0%a7%87%e0%a6%b6%e0%a6%a8%e0%a6%a8%e0%a7%8d%e0%a6%a6%e0%a6%bf%e0%a6%a8%e0%a7%80/'
-book_title = 'দুর্গেশনন্দিনী'
+book_addr = 'https://www.ebanglalibrary.com/books/%E0%A6%AE%E0%A6%A8%E0%A7%87%E0%A6%B0-%E0%A6%AE%E0%A6%BE%E0%A6%A8%E0%A7%81%E0%A6%B7-%E0%A6%B8%E0%A7%81%E0%A6%A8%E0%A7%80%E0%A6%B2-%E0%A6%97%E0%A6%99%E0%A7%8D%E0%A6%97%E0%A7%8B%E0%A6%AA%E0%A6%BE/'
+book_title = 'মনের মানুষ'
 download_book(book_addr, book_title)
 book_file.write(html_end)
 book_file.close()
+
+
+os.system(f'pandoc -f html -t epub3 -o "{main_out_filename}.epub" "{main_out_filename}.html"')
